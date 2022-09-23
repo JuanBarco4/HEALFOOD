da1284f8bc39cc7a29e893ac5acd2a123072f25a
Juan <jpbarco@scl.edu.gt>
2022-09-23 08:32:46 -0600

    Realice las interfaces de la piramide, las proteinas y lipidos para realizar sus respectivas funciones interactivas

--- js/lipido.js
+++ js/lipido.js
@@ -2,6 +2,10 @@ import {crearHeader} from './componentes/header.js';
 
 crearHeader();
 
+import { crearLip } from './componentes/lipidos.js';
+
+crearLip();
+
 import {crearFooter} from './componentes/footer.js';
 
 crearFooter();
--- js/proteina.js
+++ js/proteina.js
@@ -1,4 +1,11 @@
-
 import {crearHeader} from './componentes/header.js';
 
 crearHeader();
+
+import {crearProte} from './componentes/proteinas.js';
+
+crearProte();
+
+import { crearFooter } from './componentes/footer.js';
+
+crearFooter();
