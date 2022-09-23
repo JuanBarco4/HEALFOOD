0763dc02d61d93c45065a0d4b5de432568bbfbf5
rigoberto <rcastellanos@scl.edu.gt>
2022-09-23 10:28:46 -0600

    seccion de carbohidratos completa

--- js/carbohidra.js
+++ js/carbohidra.js
@@ -1,3 +1,8 @@
 import {crearHeader} from './componentes/header.js';
 
+
 crearHeader();
+
+
+import { crearCar } from './componentes/carbohidratos.js';
+crearCar();
--- js/componentes/carbohidratos.js
+++ js/componentes/carbohidratos.js
@@ -1,7 +1,265 @@
 export let carbohidratos = document.createElement("div");
 carbohidratos.innerHTML = 
 
-`<div id="car" class="carbo carbo2" ></div> `
+`<div class="contenedor">
+<h2 class="titulo">CARBOHIDRATOS</h2>
+
+<div class="video-container">
+    <video src=""></video>
+</div>
+
+<div class="alimentos">
+    <div class="infoPro">
+        <img class="imagen" src="img/soja-1.jpg" alt="">
+        <div class="container">
+            <div class="alimento">
+                <h3>SOJA</h3>
+                <span>413 kcal,  335 cal</span>
+            </div>
+            <div class="porcentajes">
+                <div class="prot">
+                    <h3>Proteínas</h3>
+                    <span>36%</span>
+                </div>
+                <div class="carb">
+                    <h3>Carbohidratos</h3>
+                    <span>31.3%</span>
+                </div>
+                <div class="lipi">
+                    <h3>Lípidos</h3>
+                    <span>8%</span>
+                </div>
+                
+            </div>
+            <i class="fa-solid fa-chevron-right flecha"></i>
+        </div>    
+    </div>
+    <div class="infoPro">
+        <img class="imagen" src="img/Ellipse 9.png" alt="">
+        <div class="container">
+            <div class="alimento">
+                <h3>LENTEJAS</h3>
+                <span>413 kcal,  335 cal</span>
+            </div>
+            <div class="porcentajes">
+                <div class="prot">
+                    <h3>Proteínas</h3>
+                    <span>36%</span>
+                </div>
+                <div class="carb">
+                    <h3>Carbohidratos</h3>
+                    <span>31.3%</span>
+                </div>
+                <div class="lipi">
+                    <h3>Lípidos</h3>
+                    <span>8%</span>
+                </div>
+                
+            </div>
+            <i class="fa-solid fa-chevron-right flecha"></i>
+        </div>    
+    </div>
+    <div class="infoPro">
+        <img class="imagen" src="img/POLLO.jpg" alt="">
+        <div class="container">
+            <div class="alimento">
+                <h3>POLLO</h3>
+                <span>413 kcal,  335 cal</span>
+            </div>
+            <div class="porcentajes">
+                <div class="prot">
+                    <h3>Proteínas</h3>
+                    <span>36%</span>
+                </div>
+                <div class="carb">
+                    <h3>Carbohidratos</h3>
+                    <span>31.3%</span>
+                </div>
+                <div class="lipi">
+                    <h3>Lípidos</h3>
+                    <span>8%</span>
+                </div>
+                
+            </div>
+            <i class="fa-solid fa-chevron-right flecha"></i>
+        </div>   
+</div>
+<div class="infoPro">
+    <img class="imagen" src="img/HIGADO.jpg" alt="">
+    <div class="container">
+        <div class="alimento">
+            <h3>HÍGADO</h3>
+            <span>413 kcal,  335 cal</span>
+        </div>
+        <div class="porcentajes">
+            <div class="prot">
+                <h3>Proteínas</h3>
+                <span>36%</span>
+            </div>
+            <div class="carb">
+                <h3>Carbohidratos</h3>
+                <span>31.3%</span>
+            </div>
+            <div class="lipi">
+                <h3>Lípidos</h3>
+                <span>8%</span>
+            </div>
+            
+        </div>
+        <i class="fa-solid fa-chevron-right flecha"></i>
+    </div>   
+</div>
+<div class="infoPro">
+<img class="imagen" src="img/Ellipse 12.png" alt="">
+<div class="container">
+    <div class="alimento">
+        <h3>ALMENDRAS</h3>
+        <span>413 kcal,  335 cal</span>
+    </div>
+    <div class="porcentajes">
+        <div class="prot">
+            <h3>Proteínas</h3>
+            <span>36%</span>
+        </div>
+        <div class="carb">
+            <h3>Carbohidratos</h3>
+            <span>31.3%</span>
+        </div>
+        <div class="lipi">
+            <h3>Lípidos</h3>
+            <span>8%</span>
+        </div>
+        
+    </div>
+    <i class="fa-solid fa-chevron-right flecha"></i>
+</div>   
+</div>
+<div class="infoPro">
+<img class="imagen" src="img/FRIJOLES.jpg" alt="">
+<div class="container">
+<div class="alimento">
+    <h3>FRIJOLES</h3>
+    <span>413 kcal,  335 cal</span>
+</div>
+<div class="porcentajes">
+    <div class="prot">
+        <h3>Proteínas</h3>
+        <span>36%</span>
+    </div>
+    <div class="carb">
+        <h3>Carbohidratos</h3>
+        <span>31.3%</span>
+    </div>
+    <div class="lipi">
+        <h3>Lípidos</h3>
+        <span>8%</span>
+    </div>
+    
+</div>
+<i class="fa-solid fa-chevron-right flecha"></i>
+</div>   
+</div>
+<div class="infoPro">
+<img class="imagen" src="img/ATUN.webp" alt="">
+<div class="container">
+<div class="alimento">
+    <h3>ATÚN</h3>
+    <span>413 kcal,  335 cal</span>
+</div>
+<div class="porcentajes">
+    <div class="prot">
+        <h3>Proteínas</h3>
+        <span>36%</span>
+    </div>
+    <div class="carb">
+        <h3>Carbohidratos</h3>
+        <span>31.3%</span>
+    </div>
+    <div class="lipi">
+        <h3>Lípidos</h3>
+        <span>8%</span>
+    </div>
+    
+</div>
+<i class="fa-solid fa-chevron-right flecha"></i>
+</div>   
+</div>
+<div class="infoPro">
+<img class="imagen" src="img/SALMON.jpg" alt="">
+<div class="container">
+    <div class="alimento">
+        <h3>SALMÓN</h3>
+        <span>413 kcal,  335 cal</span>
+    </div>
+    <div class="porcentajes">
+        <div class="prot">
+            <h3>Proteínas</h3>
+            <span>36%</span>
+        </div>
+        <div class="carb">
+            <h3>Carbohidratos</h3>
+            <span>31.3%</span>
+        </div>
+        <div class="lipi">
+            <h3>Lípidos</h3>
+            <span>8%</span>
+        </div>
+        
+    </div>
+    <i class="fa-solid fa-chevron-right flecha"></i>
+</div>   
+</div>
+<div class="infoPro">
+<img class="imagen" src="img/MORCILLA.jpg" alt="">
+<div class="container">
+    <div class="alimento">
+        <h3>MORCILLA</h3>
+        <span>413 kcal,  335 cal</span>
+    </div>
+    <div class="porcentajes">
+        <div class="prot">
+            <h3>Proteínas</h3>
+            <span>36%</span>
+        </div>
+        <div class="carb">
+            <h3>Carbohidratos</h3>
+            <span>31.3%</span>
+        </div>
+        <div class="lipi">
+            <h3>Lípidos</h3>
+            <span>8%</span>
+        </div>
+        
+    </div>
+    <i class="fa-solid fa-chevron-right flecha"></i>
+</div>   
+</div>
+<div class="infoPro">
+<img class="imagen" src="img/MANI.PNG" alt="">
+<div class="container">
+    <div class="alimento">
+        <h3>MANÍ</h3>
+        <span>413 kcal,  335 cal</span>
+    </div>
+    <div class="porcentajes">
+        <div class="prot">
+            <h3>Proteínas</h3>
+            <span>36%</span>
+        </div>
+        <div class="carb">
+            <h3>Carbohidratos</h3>
+            <span>31.3%</span>
+        </div>
+        <div class="lipi">
+            <h3>Lípidos</h3>
+            <span>8%</span>
+        </div>
+        
+    </div>
+    <i class="fa-solid fa-chevron-right flecha"></i>
+</div>   
+</div>
+</div>`
 
 
     export function crearCar (){
@@ -10,7 +268,7 @@ carbohidratos.innerHTML =
          // ELEMENTOS DEL DOM
          let div_car = document.querySelector("#root");
     
-         div_car.appendChild(crearCar);
+         div_car.appendChild(carbohidratos);
          // 
  
     
