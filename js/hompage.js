6fd1b6172b3893b173dca0a48cf4afc005455a7e
rigoberto <rcastellanos@scl.edu.gt>
2022-09-27 18:44:17 -0600

    actualizacion de la homepage

--- js/hompage.js
+++ js/hompage.js
@@ -2,5 +2,10 @@ import {crearHeader} from './componentes/header.js';
 crearHeader();
 import { crearInfo } from './componentes/menu.js ';
 crearInfo();
+import { crearPiramide } from './componentes/piramide.js';
+
+crearPiramide();
+import { crearEsta } from './componentes/estadistica.js';
+crearEsta();
 import {crearFooter} from './componentes/footer.js';
 crearFooter();
